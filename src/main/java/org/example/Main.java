2b1faf2c094fefd63bc014d6d5e1c19c506ec1b8
AndreiVelmezev <velmezev@gmail.com>
2022-08-12 23:49:14 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
